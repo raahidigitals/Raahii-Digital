2059ad753f566814684a294a54ce0d30139afcac
raahidigitals <raahidigitals@gmail.com>
2026-09-23 03:57:31 +0530

    Fix GTM

--- app/layout.tsx
+++ app/layout.tsx
@@ -78,47 +78,48 @@ export default async function RootLayout({
   return (
     <html lang="en">
       <head>
-        {/* Google Tag Manager */}
-        <Script
-          id="google-tag-manager"
-          strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `
-              (function(w,d,s,l,i){
-                w[l]=w[l]||[];
-                w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});
-                var f=d.getElementsByTagName(s)[0],
-                j=d.createElement(s),
-                dl=l!='dataLayer'?'&l='+l:'';
-                j.async=true;
-                j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
-                f.parentNode.insertBefore(j,f);
-              })(window,document,'script','dataLayer','GTM-MMD575FG');
-            `,
-          }}
-        />
-      </head>
+  <Script
+    id="google-tag-manager"
+    strategy="afterInteractive"
+    dangerouslySetInnerHTML={{
+      __html: `
+        (function(w,d,s,l,i){
+          w[l]=w[l]||[];
+          w[l].push({
+            'gtm.start': new Date().getTime(),
+            event:'gtm.js'
+          });
+          var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),
+              dl=l!='dataLayer'?'&l='+l:'';
+          j.async=true;
+          j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
+          f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-NWD575FG');
+      `,
+    }}
+  />
+</head>
 
       <body className="min-h-screen antialiased">
         {/* Google Tag Manager (noscript) */}
         <noscript>
-          <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-MMD575FG"
-            height="0"
-            width="0"
-            style={{
-              display: "none",
-              visibility: "hidden",
-            }}
-          />
-        </noscript>
+  <iframe
+    src="https://www.googletagmanager.com/ns.html?id=GTM-NWD575FG"
+    height="0"
+    width="0"
+    style={{
+      display: "none",
+      visibility: "hidden",
+    }}
+  />
+</noscript>
 
         <Navbar settings={siteSettings} />
 
         {children}
 
         <Footer settings={siteSettings} />
-
         <FloatingCTA settings={siteSettings?.floatingCTA} />
       </body>
     </html>
